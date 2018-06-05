e5937514c7120f91b816e7458a591cf69fc7f13d
AdityaGupta1 <adityag@gmail.com>
2018-06-04 23:48:23 -0700

    fixed #12

---
+++ scenes/health bar.gd
@@ -0,0 +1,7 @@
+extends ColorRect
+
+onready var player = get_tree().get_root().get_node("Main").get_node("Player");
+
+func _process(delta):
+	rect_size.y = 400.0 * player.health / player.max_health;
+	pass;
