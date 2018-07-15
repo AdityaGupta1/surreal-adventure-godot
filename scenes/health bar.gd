b53a02d41e93b08216c47e333d2e147211bbd557
AdityaGupta1 <adityag@gmail.com>
2018-07-15 15:00:17 -0700

    fixed #6

--- scenes/health bar.gd
+++ scenes/health bar.gd
@@ -6,4 +6,6 @@ func _process(delta):
 	if main.has_node("Player"):
 		var player = main.get_node("Player");
 		rect_size.y = 400.0 * player.health / player.max_health;
+	else:
+		rect_size.y = 0;
 	
