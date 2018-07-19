830fff0cd1e57c500703f2f58e5e544a82fe1f9c
AdityaGupta1 <adityag@gmail.com>
2018-07-19 16:55:23 -0700

    fixed #27

--- scenes/pause.gd
+++ scenes/pause.gd
@@ -1,17 +1,24 @@
 extends Control
 
-var paused;
+var _paused;
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_pause"):
 		_toggle_paused();
+		
+func _notification(event):
+	if event == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+		set_paused(true);
 
-func _toggle_paused():
-	paused = !paused;
+func set_paused(new_paused):
+	_paused = new_paused;
 	
-	get_tree().paused = paused;
+	get_tree().paused = _paused;
 	
-	if paused:
+	if _paused:
 		show();
 	else:
 		hide();
+
+func _toggle_paused():
+	set_paused(!_paused);
