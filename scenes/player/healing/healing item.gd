f485d9ec3e78e7087513df6a3b9838e28b95e827
AdityaGupta1 <adityag@gmail.com>
2018-10-10 13:53:49 -0700

    fixed #78

--- scenes/player/healing/healing item.gd
+++ scenes/player/healing/healing item.gd
@@ -1,5 +1,8 @@
 extends RigidBody
 
+var time = 0;
+var pickup_delay = 1;
+
 var health_restored = {
 	"bepis can": 15,
 	"earth": 25,
@@ -11,10 +14,12 @@ func _ready():
 	set_collision_mask(33); # player and enemies
 	
 func _on_body_entered(body):
-	if body.get_name() == "Player":
+	if body.get_name() == "Player" and time > pickup_delay:
 		get_tree().get_root().get_node("Main").get_node("Player").heal(health_restored[self.get_name()]);
 		queue_free();
 	
 func _physics_process(delta):
+	time += delta;
+	
 	if global_transform.origin.y < 0:
 		get_parent().queue_free(); # because each healing item is encased in a Spatial to preserve its name
