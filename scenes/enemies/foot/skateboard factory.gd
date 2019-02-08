41d79b8dad1bc331103c0e97a90af0168a4e892b
AdityaGupta1 <adityag@gmail.com>
2019-02-07 17:12:04 -0800

    fixed #87

--- scenes/enemies/foot/skateboard factory.gd
+++ scenes/enemies/foot/skateboard factory.gd
@@ -1,11 +1,11 @@
 extends "res://scenes/enemies/bullet factory.gd"
 
 func _ready():
-	shoot_towards = "player";
+	shoot_towards = "outward";
 	shoot_angle = -90;
 	
 	bullet = preload("res://scenes/enemies/foot/skateboard.tscn");
-	shoot_delay = 9.0/11.0;
+	shoot_delay = 7.0/13.0;
 	
 	shotgun_bullets = 3;
 	shotgun_angle = 20;
